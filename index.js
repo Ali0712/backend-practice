cc8b128b3beac0c0076556abfeb6a6ef99fdfe7f
Muhammad Ali <muhammadali07122@gmail.com>
2024-03-03 19:24:12 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
+
+// get all movies
+app.get('/api/v1/movies', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        count: movies.length,
+        data: { movies: movies }
+    })
+})
+
+// get movie by id
+app.get('/api/v1/movies/:id', (req, res) => {
+    const id = +req.params.id;
+    const movie = movies.find(data => data.id === id);
+    if (!movie) {
+        return res.status(404).json({
+            status: 'fail',
+            message: `movie with id = ${id} not found`
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            movie: movie
+        }
+    })
+})
+
+// add new movie
+app.post('/api/v1/movies', (req, res) => {
+    const newID = movies[movies.length - 1].id + 1;
+    const newMovie = Object.assign({ 'id': newID }, req.body)
+    movies.push(newMovie)
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
+        res.status(201).json({
+            status: "success",
+            data: { movie: newMovie }
+        })
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
